106381c7e3b25c266493a8ba0c32c48d665f8d00
Nick Ren <zhong.ren@hotmail.com>
2025-04-08 11:05:07 -0400

    chore(script): add git pull step to update before processing

--- update.sh
+++ update.sh
@@ -3,10 +3,14 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd "$DIR"
 
+# Pull latest changes before starting work
+git pull
+
+# Run data processing steps
 bash download.sh
 python3 process.py
 
-# push the changes to remote repository
+# Push the changes to remote repository
 git add data/output/*.csv
 git commit -m "Weekly update"
 git push
